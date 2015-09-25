60ae6d5c7cf64517bc318ccc3130beaa99f06a75
songjie <songjiesdnu@163.com>
2015-09-25 22:21:37 +0800

    初始化

---
+++ src/main/java/com/smarttrip/domain/User.java
@@ -0,0 +1,103 @@
+package com.smarttrip.domain;
+
+public class User {
+    private String id;
+
+    private String name;
+
+    private String password;
+
+    private String salt;
+
+    private String realName;
+
+    private String nickName;
+
+    private String email;
+
+    private String mobileNo;
+
+    private String registerTime;
+
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(String registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/smarttrip/service/IUserService.java
@@ -0,0 +1,28 @@
+/**
+ * Date:2015年7月30日下午11:20:52
+ * Copyright (c) 2015, songjiesdnu@163.com All Rights Reserved.
+ */
+package com.smarttrip.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+
+import com.smarttrip.domain.User;
+
+/**
+ * 
+ * @author songjie
+ */
+public interface IUserService {
+	
+	int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByPrimaryKey(User record);
+    
+    List<User> selectByPage(RowBounds rb);
+}
