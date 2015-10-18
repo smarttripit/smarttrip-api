bb8972ea1f8c717c794f7add826e2534349f8aeb
gaoweibupt <gaoweibupt@163.com>
2015-10-18 13:47:49 +0800

    更新XXX.java文件

---
+++ src/main/java/com/smarttrip/domain/VisitorTheme.java
@@ -0,0 +1,33 @@
+package com.smarttrip.domain;
+
+public class VisitorTheme {
+    private String id;
+
+    private String visitorId;
+
+    private String themeId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getVisitorId() {
+        return visitorId;
+    }
+
+    public void setVisitorId(String visitorId) {
+        this.visitorId = visitorId;
+    }
+
+    public String getThemeId() {
+        return themeId;
+    }
+
+    public void setThemeId(String themeId) {
+        this.themeId = themeId;
+    }
+}
